8ffb6045268c86cc65fb4e956804c1d2d755f077
fourhanz <farhankreenz@gmail.com>
2021-07-04 11:29:22 +0700

    Testing

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
@@ -11,4 +11,5 @@ const App = () => {
   );
 };
 
+const styles = StyleSheet.create({});
 export default App;
